a07c8929c80d290ec64ccc608129b11f67b0922a
hydai <z54981220@gmail.com>
2015-03-25 09:17:53 +0800

    Move travis-ci test script to travis-ci folder

---
+++ travis-ci/runTest.sh
@@ -0,0 +1,10 @@
+mysql -e 'create database IF NOT EXISTS test_db;' -uroot
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';" -uroot
+python install.py < info.txt
+cat emailInfo.py
+cat nthuoj.ini
+python manage.py bower install
+python manage.py makemigrations
+python manage.py migrate
+#python manage.py createsuperuser
+python manage.py test
